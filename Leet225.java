c47b5990bb09b6282018c7515a9cd80eb995a55d
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2025-08-05 10:18:46 +0530

    changes

--- Leet225.java
+++ Leet225.java
@@ -1,57 +1,63 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class MyStack {
-    private Queue<Integer> main;
-    private Queue<Integer> helper;
+public class Leet225 {
+    class MyStack {
+        private Queue<Integer> main;
+        private Queue<Integer> helper;
 
-    public MyStack() {
-        main = new LinkedList<>();
-        helper = new LinkedList<>();
-    }
-
-    public void push(int x) {
-        while (!main.isEmpty()) {
-            helper.add(main.remove());
+        public MyStack() {
+            main = new LinkedList<>();
+            helper = new LinkedList<>();
         }
-        main.add(x);
-        while (!helper.isEmpty()) {
-            main.add(helper.remove());
-        }
-    }
 
-    public int pop() {
-        if (main.isEmpty()) {
-            throw new RuntimeException("Stack is empty");
+        public void push(int x) {
+            while (!main.isEmpty()) {
+                helper.add(main.remove());
+            }
+            main.add(x);
+            while (!helper.isEmpty()) {
+                main.add(helper.remove());
+            }
         }
-        return main.remove();
-    }
 
-    public int top() {
-        if (main.isEmpty()) {
-            throw new RuntimeException("Stack is empty");
+        public int pop() {
+            if (main.isEmpty()) {
+                throw new RuntimeException("Stack is empty");
+            }
+            return main.remove();
         }
-        return main.peek();
-    }
 
-    public boolean empty() {
-        return main.isEmpty();
+        public int top() {
+            if (main.isEmpty()) {
+                throw new RuntimeException("Stack is empty");
+            }
+            return main.peek();
+        }
+
+        public boolean empty() {
+            return main.isEmpty();
+        }
     }
 
     public static void main(String[] args) {
-        MyStack stack = new MyStack();
+
+        Leet225 outer = new Leet225();
+
+     
+        MyStack stack = outer.new MyStack();
 
         stack.push(10);
         stack.push(20);
         stack.push(30);
 
-        System.out.println("Top element: " + stack.top());   
-        System.out.println("Popped element: " + stack.pop()); 
-        System.out.println("Top element after pop: " + stack.top()); 
-        System.out.println("Is stack empty? " + stack.empty()); 
+        System.out.println("Top element: " + stack.top());
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Top element after pop: " + stack.top());
+        System.out.println("Is stack empty? " + stack.empty());
 
-        stack.pop(); 
-        stack.pop(); 
+        stack.pop();
+        stack.pop();
 
         System.out.println("Is stack empty now? " + stack.empty());
     }
